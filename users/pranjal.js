7540de607c5f71f988259da8ead4444cda5c503d
Sudhansu Sekhar Behera <118283474+skehargit@users.noreply.github.com>
2024-02-13 09:58:01 +0530

    progress bar update

--- users/pranjal.js
+++ users/pranjal.js
@@ -12,9 +12,21 @@ var pranjal=[
         duration: "3.19",
       },
       {
-        name: "Top of cliff",
-        url: "./songs/kuch to hai.mp3",
+        name: "Top or cliff",
+        url: "./songs/Top or Cliff.mp3",
         img: "./images/top of cliff.jpg",
-        duration: "4.08",
+        duration: "3.41",
+      },
+      {
+        name: "I Love my body",
+        url: "./songs/I Love My Body.mp3",
+        img: "./images/i love my body.jpg",
+        duration: "2.30",
+      },
+      {
+        name: "7 rings",
+        url: "./songs/7 rings.mp3",
+        img: "./images/7 rings.png",
+        duration: "2.58",
       }
 ]
